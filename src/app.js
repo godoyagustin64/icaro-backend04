17c14394ef17cf3f2cd4b31e07e6d47addac3ed0
Agustin Godoy <godoyagustinnahuel93@gmail.com>
2022-07-18 20:37:58 -0300

    se añade ejs

--- src/app.js
+++ src/app.js
@@ -3,11 +3,19 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
     res.send("Bienvenido a la pagina principal!")
 });
 
+app.get("/index", (req, res) => {
+    res.render("index")
+});
+
 // Registrar como publica la carpeta public
 app.use(express.static("public"))
+app.use("/prefijo", express.static("publica"))
 
 app.listen(3000, () => console.log("Proyecto en el puerto 3000"))
